59212d9fd066969a8371a8f073628d456016b574
Teala-B <bradleyteala@gmail.com>
2023-11-15 14:20:43 -0500

    got the parks to show and filter

--- scripts/locationData.js
+++ scripts/locationData.js
@@ -65,5 +65,56 @@ document.addEventListener("DOMContentLoaded", ()=>{
         option.textContent = locationsArray[i];
         selectLocation.appendChild(option)
     }
+
+    displayParks(nationalParksArray)
 })
 
+function filterByState() {
+    //this code fires everytime they make a change
+    // let selectLocation = document.getElementById("selectLocation")
+    // let selectedIndex = selectLocation.selectedIndex;
+    // let selectedOption = selectLocation.options[selectedIndex];
+
+    let selected = document.getElementById("selectLocation").value;
+    const parks = [];
+    for(i=0; i< nationalParksArray.length; i++){
+        if(nationalParksArray[i].State == selected){
+            parks.push(nationalParksArray[i]);
+        }
+    }
+    displayParks(parks)
+}
+
+function displayParks(parks) {
+    const parentDiv = document.getElementById("displayParks")
+    parentDiv.innerText = ""
+
+    parks.forEach(park => {
+        individulPark(park, parentDiv)
+    });
+        
+} 
+
+function individulPark(park, parentDiv) {
+    // create a div for each product
+    const parkDiv = document.createElement("div");
+    parkDiv.classList.add("product") ;
+    // add product to the container
+    parentDiv.appendChild(parkDiv);
+
+    addName(park, parkDiv);
+    addAddress(park, parkDiv);
+    // addContact(park, parkDiv);
+    // addSite(park, parkDiv);
+}
+
+function addName(park, parkDiv) {
+    // create the product info div 
+    const parkInfoDiv = document.createElement("div");
+    parkDiv.appendChild(parkInfoDiv);
+
+    // add product header
+    const parkName = document.createElement("h5")
+    parkName.innerText = park.LocationName;
+    parkInfoDiv.appendChild(parkName);
+}
