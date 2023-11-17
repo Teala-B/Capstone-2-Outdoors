df229f57021cc8323f02b8c0a67d3d7029564be7
Teala-B <bradleyteala@gmail.com>
2023-11-17 14:08:44 -0500

    got description to show

--- scripts/mountainData.js
+++ scripts/mountainData.js
@@ -588,3 +588,8 @@ function getMountainImage(mountain, mountainInfoDiv) {
     mountainInfoDiv.appendChild(img);
 }
 
+function getMountainDescription(mountain, mountainInfoDiv) {
+    const mountainDescription = document.createElement("p");
+    mountainDescription.innerText = mountain.desc;
+    mountainInfoDiv.appendChild(mountainDescription);
+}
